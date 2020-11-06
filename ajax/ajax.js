7c12c01e9d756e91b7357534172165281517eb82
Kali Correll <kalicorrell18@gmail.com>
2020-11-06 12:25:20 -0500

    Updated usercontroller

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,8 +1,32 @@
 //jQuery ready function
 $(() => {
+
+    var User ={
+    iD: 0, userName: "Kcorrell", password: "Login", firstName: "Kali", lastName: "Correll",
+    phoneNumber: "5132952715", email: "test@email.com", isReviewer: false, isAdmin: false
+};
+
+$.ajax({
+    method: "POST",
+    url:"http://localhost:8080/api/users/",
+    data: JSON.stringify(User),
+    contentType: "application/json"
+
+})
+.done(res => {
+    console.log(res);
+    getAll();
+})
+.fail(err => {
+    console.error(err);
+});
+
+const getAll = () => {
 $.getJSON("http://localhost:8080/api/users/")
-.done(users=>{
+.done(users => {
     console.log(users);
 });
+}
 console.log("After the getJSON call");
-    })
+getAll();
+})
